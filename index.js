45617c8708893c95b4a8e82f0149680200c40ea4
Eric Zimmer <zimmer.eric@gmail.com>
2018-04-23 23:18:27 -0700

    node test app

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 
-function run (msg, num){
-  for (let i = 0; i < num; i++) {
-    console.log(i + ' ' + msg);
-    
-  }
- 
-}
+var express = require('express');
+var app = express();
 
-run("Hey there", 10);
-run("ohai", 3);
+app.get("/", function(req, res){
+  res.send("hi there weclome to the test!");
+});
+
+app.get("/speak/:animal", (req, res)=> {
+  var animal = req.params.animal;
+  var str = "The " + animal + " says ";
+  if (animal === "pig"){
+    str += "OINK";
+  }
+  else if(animal === "cow"){
+    str += "MOOOO";
+  }
+  else if(animal === "dog"){
+    str += "WOOF";
+  }
+  res.send(str);
+});
+
+app.get("/repeat/:word/:num", (req, res) => {
+  var word = req.params.word, 
+  num = parseInt(req.params.num),
+  str = "";
+
+  for (var i = 0; i<num; i++){
+    str += word + ' ';
+  }
+  res.send(str);
+
+
+});
+
+app.get("*",(req,res)=>{
+  res.send("WAAAAAAAAAAAAAAT");
+});
+
+app.listen(3000, () => {
+  console.log("runs fine!");
+});
